5c33ed05e85c689b0759594ce92e1fe6106c64f9
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2024-05-22 23:42:51 +0200

    Add logic for number past 100 & 1000

--- num_converter.py
+++ num_converter.py
@@ -1,36 +1,69 @@
 """
+Convert numbers to French
 """
 
 CONSTANTS = {
-    0: "zéro", 1: "un", 2: "deux", 3: "trois", 4: "quatre", 5: "cinq",
-    6: "six", 7: "sept", 8: "huit", 9: "neuf", 10: "dix", 11: "onze",
-    12: "douze", 13: "treize", 14: "quatorze", 15: "quinze", 16: "seize",
+    0: "zéro", 1: "un", 2: "deux", 3: "trois", 4: "quatre",
+    5: "cinq", 6: "six", 7: "sept", 8: "huit", 9: "neuf",
+    10: "dix", 11: "onze", 12: "douze", 13: "treize",
+    14: "quatorze", 15: "quinze", 16: "seize",
+    17: "dix-sept", 18: "dix-huit", 19: "dix-neuf",
     20: "vingt", 30: "trente", 40: "quarante", 50: "cinquante", 60: "soixante",
     70: "soixante-dix", 80: "quatre-vingts", 90: "quatre-vingt-dix",
     100: "cent", 1000: "mille"
 }
 
-def convert_num_to_french(num):
-    num_in_french = ""
+def convert_0_to_99(number):
+    """Convert numbers from 0 to 99"""
 
-    if num in CONSTANTS:
-        num_in_french = CONSTANTS[num]
-    else:
-        if num < 70:
-            remainder = num % 10
-            if remainder == 1:
-                num_in_french = f"{CONSTANTS[num - remainder]}-et-un"
-            else:
-                num_in_french = f"{CONSTANTS[num - remainder]}-{CONSTANTS[remainder]}"
+    number_in_french = ""
+
+    if number < 70:
+        remainder = number % 10
+        if remainder == 1:
+            number_in_french = f"{CONSTANTS[number - remainder]}-et-un"
         else:
-            if (num > 70) and (num < 80):
-                num_in_french = f"{CONSTANTS[60]}-{CONSTANTS[num - 60]}"
-            elif (num > 80) and (num < 100):
-                num_in_french = f"{CONSTANTS[80][:-1]}-{CONSTANTS[num - 80]}"
+            number_in_french = f"{CONSTANTS[number - remainder]}-{CONSTANTS[remainder]}"
+    else:
+        if (number > 70) and (number < 80):
+            number_in_french = f"{CONSTANTS[60]}-{CONSTANTS[number - 60]}"
+        elif (number > 80) and (number < 100):
+            number_in_french = f"{CONSTANTS[80][:-1]}-{CONSTANTS[number - 80]}"
 
-    return(num_in_french)
+    return number_in_french
+
+
+def convert_100_to_999(number):
+    """Convert numbers from 100 to 999"""
+
+    floor = number // 100
+    if floor == 1:
+        return f"{CONSTANTS[100]} {convert_0_to_99(number % 100)}"
+    return f"{CONSTANTS[floor]} {CONSTANTS[100]} {convert_0_to_99(number % 100)}"
+
+
+def convert_1000_upwards(number):
+    """Convert numbers from 1000 going upwards"""
+
+    floor = number // 1000
+    if floor == 1:
+        return f"{CONSTANTS[1000]} {convert_100_to_999(number % 100)}"
+    return f"{CONSTANTS[floor]} {CONSTANTS[1000]} {convert_1000_upwards(number % 1000)}"
+
+
+def convert_number_to_french(number):
+    """Convert a given number n to French"""
+
+    if number in CONSTANTS:
+        return CONSTANTS[number]
+    elif number > 999:
+        return convert_1000_upwards(number)
+    elif number > 100:
+        return convert_100_to_999(number)
+    elif number > 10:
+        return convert_0_to_99(number)
 
 
 if __name__=="__main__":
-    n = 87
-    print(convert_num_to_french(n))
+    n = 197
+    print(convert_number_to_french(n))
